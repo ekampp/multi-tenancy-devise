8696c9e90a07bb3f64dc720b71ab61eb5cd95f63
Emil Kampp <emil@kampp.me>
2018-03-07 10:40:28 +0100

    Update assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,2 @@
 Rails.application.config.assets.version = '1.0'
-Rails.application.config.assets.precompile += %w( mtdevise/jumbotron-narrow.css mtdevise/non-responsive.css)
+# Rails.application.config.assets.precompile += %w( mtdevise/jumbotron-narrow.css mtdevise/non-responsive.css )
